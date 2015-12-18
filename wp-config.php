a9ddf5cd6c010616bfe9a87e42ce84409a82a5ad
Rachel Whitton <rachel@pantheon.io>
2015-12-18 21:24:39 +0000

    Revise https redirect logic in wp-config.php to align with docs

--- wp-config.php
+++ wp-config.php
@@ -87,15 +87,16 @@ else:
 
     }
     // Require SSL.
-if (isset($_SERVER['PANTHEON_ENVIRONMENT']) &&
-  $_SERVER['PANTHEON_ENVIRONMENT'] === 'live') {
-  if (!isset($_SERVER['HTTP_X_SSL']) ||
-    (isset($_SERVER['HTTP_X_SSL']) && $_SERVER['HTTP_X_SSL'] != 'ON')) {
-    header('HTTP/1.0 301 Moved Permanently');
-    header('Location: https://rachelwhitton.com'. $_SERVER['REQUEST_URI']);
-    exit();
-  }
-}
+    // Require HTTPS.
+    if (isset($_SERVER['PANTHEON_ENVIRONMENT']) &&
+      $_SERVER['HTTPS'] === 'OFF') {
+      if (!isset($_SERVER['HTTP_X_SSL']) ||
+        (isset($_SERVER['HTTP_X_SSL']) && $_SERVER['HTTP_X_SSL'] != 'ON')) {
+        header('HTTP/1.0 301 Moved Permanently');
+        header('Location: https://'. $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI']);
+        exit();
+      }
+    }
 
     if (isset($_SERVER['PANTHEON_ENVIRONMENT']) &&
       $_SERVER['PANTHEON_ENVIRONMENT'] === 'live') {
