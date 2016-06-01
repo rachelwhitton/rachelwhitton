b1ac3df8943247b0c1be33171ba1ff874f1dd14d
Rachel <rachel@getpantheon.com>
2016-06-01 15:09:30 -0500

    Comment out upstream config for wp_debug

--- wp-config.php
+++ wp-config.php
@@ -144,9 +144,9 @@ define('WPLANG', '');
  * You may want to examine $_ENV['PANTHEON_ENVIRONMENT'] to set this to be
  * "true" in dev, but false in test and live.
  */
-if ( ! defined( 'WP_DEBUG' ) ) {
-    define('WP_DEBUG', false);
-}
+#if ( ! defined( 'WP_DEBUG' ) ) {
+#    define('WP_DEBUG', false);
+#}
 
 /* That's all, stop editing! Happy Pressing. */
 
@@ -155,19 +155,23 @@ if (defined('PANTHEON_ENVIRONMENT')) {
   //Wordpress debug settings in development environments.
   if (!in_array(PANTHEON_ENVIRONMENT, array('test', 'live'))) {
     // Debugging enabled.
-    define( 'WP_DEBUG', true );
-    define( 'WP_DEBUG_LOG', true ); // Stored in wp-content/debug.log
-    define( 'WP_DEBUG_DISPLAY', true );
+    if ( ! defined( 'WP_DEBUG' ) ) {
+      define( 'WP_DEBUG', true );
+      define( 'WP_DEBUG_LOG', true ); // Stored in wp-content/debug.log
+      define( 'WP_DEBUG_DISPLAY', true );
+    }
   }
   // Wordpress debug settings in test and live environments.
   else {
     // Debugging disabled
-    ini_set('log_errors','On');
-    ini_set('display_errors','Off');
-    ini_set('error_reporting', E_ALL );
-    define('WP_DEBUG', false);
-    define('WP_DEBUG_LOG', true);
-    define('WP_DEBUG_DISPLAY', false);
+    if ( ! defined( 'WP_DEBUG' ) ) {
+      ini_set('log_errors','On');
+      ini_set('display_errors','Off');
+      ini_set('error_reporting', E_ALL );
+      define('WP_DEBUG', false);
+      define('WP_DEBUG_LOG', true);
+      define('WP_DEBUG_DISPLAY', false);
+    }
   }
 }
 
